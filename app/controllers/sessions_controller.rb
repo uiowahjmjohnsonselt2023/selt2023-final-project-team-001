7b1467834d2314549c6566616876a1b34b7a55b0
blhohmann <blake-hohmann@uiowa.edu>
2023-11-07 15:00:14 -0600

    feat: add initial code for model, view, controller for login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+  def new
+    # automatically renders the sign-in form
+  end
+
+  def create
+    # Handle sign-in and authentication logic
+    user = User.find_by(email: params[:session][:email])
+
+    if user&.user&.authenticate(params[:session][:password])
+      # Sign in the user
+      session[:user_id] = user.id
+      flash[:notice] = "Successfully signed in!"
+      redirect_to "/", notice: "Successfully signed in!"
+    else
+      flash.now[:alert] = "Invalid email/password combination"
+      render "new"
+    end
+  end
+
+  def destroy
+    # Handle sign-out logic
+    session[:user_id] = nil
+    flash[:notice] = "Signed out successfully!"
+    redirect_to "/", notice: "Signed out successfully!"
+  end
+end
