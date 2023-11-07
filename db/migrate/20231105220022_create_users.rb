f604506e06507f3051acc19c0cedca6480c0792c
powersaudrey25 <powersaudrey25@gmail.com>
2023-11-07 17:10:31 -0600

    refactor: switch to change, add is_admin attr

--- db/migrate/20231105220022_create_users.rb
+++ db/migrate/20231105220022_create_users.rb
@@ -1,5 +1,5 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
-  def up
+  def change
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
@@ -8,11 +8,8 @@ class CreateUsers < ActiveRecord::Migration[7.1]
       t.string :password_digest_confirmation
       t.boolean :is_seller, null: false, default: false
       t.boolean :is_buyer, null: false, default: false
+      t.boolean :is_admin, null: false, default: false
       t.timestamps
     end
-  end
-
-  def down
-    drop_table :users
   end
 end
