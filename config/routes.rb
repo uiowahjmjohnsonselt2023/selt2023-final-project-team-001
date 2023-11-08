6d74a4e3c02b2c6a5222381f4d413003440b4849
blhohmann <blake-hohmann@uiowa.edu>
2023-11-07 22:09:46 -0600

    chore: add path for submitting login for convenience for ease of testing and use

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users
 
   get "/login", to: "sessions#new", as: "login"
-  post "/login", to: "sessions#create"
+  post "/login", to: "sessions#create", as: "login_submit"
   delete "/logout", to: "sessions#destroy", as: "logout"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
