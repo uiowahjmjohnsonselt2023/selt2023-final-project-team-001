44e1c740119bf5b3a5c3777f91b7633df15ca23a
evfinkn <evan@finken.name>
2023-11-09 14:40:15 -0600

    feat: add scopes to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+
+  # Scopes allow us to write things like User.admins to get all admins
+  # or User.non_sellers to get all users who aren't sellers.
+  scope :admins, -> { where(is_admin: true) }
+  scope :non_admins, -> { where(is_admin: false) }
+  scope :sellers, -> { where(is_seller: true) }
+  scope :non_sellers, -> { where(is_seller: false) }
+  scope :buyers, -> { where(is_buyer: true) }
+  scope :non_buyers, -> { where(is_buyer: false) }
 end
