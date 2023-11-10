0bf22c9fd5a318ad98809d310e3808c816e8ed6b
blhohmann <blake-hohmann@uiowa.edu>
2023-11-10 11:38:41 -0600

    feat: add validation to profile's website, twitter, facebook, instagram links

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,11 @@ class Profile < ApplicationRecord
   belongs_to :user
   mount_uploader :avatar, AvatarUploader
 
+  validates :website, format: {with: URI::DEFAULT_PARSER.make_regexp, message: "is not a valid URL"}, allow_blank: true
+  validates :twitter, format: {with: %r{\A(?:https?://)?(?:www\.)?twitter\.com/\w+\z}, message: "is not a valid Twitter link"}, allow_blank: true
+  validates :facebook, format: {with: %r{\A(?:https?://)?(?:www\.)?facebook\.com/\w+\z}, message: "is not a valid Facebook link"}, allow_blank: true
+  validates :instagram, format: {with: %r{\A(?:https?://)?(?:www\.)?instagram\.com/\w+\z}, message: "is not a valid Instagram link"}, allow_blank: true
+
   def full_name
     "#{first_name} #{last_name}"
   end
