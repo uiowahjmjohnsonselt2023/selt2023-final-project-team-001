e176fa13eeb33fdd668fdc57eb47e670c36f8cfc
evfinkn <evan@finken.name>
2023-11-09 21:50:34 -0600

    feat: create new and edit forms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,42 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+    @product.categorizations.build
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.seller = User.first
+    if @product.save
+      redirect_to @product
+    else
+      flash[:alert] = "Please fix the errors below."
+      render "new"
+    end
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      flash[:alert] = "Please fix the errors below."
+      render "edit"
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name, :description, :price, :quantity, :condition, :private
+    )
+  end
 end
