6b2b2bd404d84fb67e0b9f73e14ec4071bb5cab9
powersaudrey25 <powersaudrey25@gmail.com>
2023-11-09 19:27:22 -0600

    fix: create a valid user to fix failing tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,7 +3,14 @@
 require "rails_helper"
 
 describe SessionsController, type: :controller do
-  let(:valid_user) { create(:user, email: "admin@admin.com", password: "admin") }
+  let(:valid_user) {
+    create(:user,
+      first_name: "test",
+      last_name: "test",
+      email: "admin@admin.com",
+      password: "admin000",
+      password_confirmation: "admin000")
+  }
 
   describe "POST #create" do
     context "with valid credentials" do
@@ -32,22 +39,19 @@ describe SessionsController, type: :controller do
 
   describe "DELETE #destroy" do
     it "signs out the user" do
-      user = create(:user)
-      session[:user_id] = user.id
+      session[:user_id] = valid_user.id
       delete :destroy
       expect(session[:user_id]).to be_nil
     end
 
     it "redirects to the root after logging in" do
-      user = create(:user)
-      session[:user_id] = user.id
+      session[:user_id] = valid_user.id
       delete :destroy
       expect(response).to redirect_to("/")
     end
 
     it "displays a success flash message after logging in" do
-      user = create(:user)
-      session[:user_id] = user.id
+      session[:user_id] = valid_user.id
       delete :destroy
       expect(flash[:notice]).to eq("Signed out successfully!")
     end
