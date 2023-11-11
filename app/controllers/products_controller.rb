c7427bac9e442fceacf42cdae8b7c620dddba447
evfinkn <evan@finken.name>
2023-11-10 20:44:39 -0600

    feat: add permissions to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,30 @@
 class ProductsController < ApplicationController
+  before_action :set_current_user, except: [:show]
+
   def show
     @product = Product.find(params[:id])
+    if @product.private && current_user != @product.seller && !current_user&.is_admin
+      flash[:alert] = "You don't have permission to view that product."
+      redirect_to root_path
+    end
   end
 
   def new
+    unless current_user.is_seller || current_user.is_admin
+      flash[:alert] = "You must be a seller to create a product."
+      redirect_to root_path and return
+    end
     @product = Product.new
     @product.categorizations.build
   end
 
   def create
+    unless current_user.is_seller || current_user.is_admin
+      flash[:alert] = "You must be a seller to create a product."
+      redirect_to root_path and return
+    end
     @product = Product.new(product_params)
-    @product.seller = User.first
+    @product.seller = current_user
     if @product.save
       redirect_to @product
     else
@@ -21,11 +35,18 @@ class ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+    unless current_user == @product.seller || current_user.is_admin
+      flash[:alert] = "You don't have permission to edit that product."
+      redirect_to root_path
+    end
   end
 
   def update
     @product = Product.find(params[:id])
-    if @product.update(product_params)
+    if current_user != @product.seller && !current_user.is_admin
+      flash[:alert] = "You don't have permission to edit that product."
+      redirect_to root_path
+    elsif @product.update(product_params)
       redirect_to @product
     else
       flash[:alert] = "Please fix the errors below."
