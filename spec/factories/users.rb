40a4dd9bfe95361cd69d098ba839b1c09c13c3f9
evfinkn <evan@finken.name>
2023-11-10 21:00:03 -0600

    test: add tests for Product and its controller

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,17 @@
-# spec/factories/users.rb
 FactoryBot.define do
   factory :user do
-    first_name { "test" }
-    last_name { "test" }
-    email { "test@example.com" }
+    sequence(:first_name) { |n| "test#{n}" }
+    sequence(:last_name) { |n| "user#{n}" }
+    sequence(:email) { |n| "person#{n}@example.com" }
     password { "password123" }
     password_confirmation { "password123" }
+
+    trait(:seller) { is_seller { true } }
+    trait(:buyer) { is_buyer { true } }
+    trait(:admin) { is_admin { true } }
+
+    factory :seller, traits: [:seller]
+    factory :buyer, traits: [:buyer]
+    factory :admin, traits: [:admin]
   end
 end
--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,24 +1,5 @@
 require "rails_helper"
 
 RSpec.describe "Products", type: :request do
-  describe "GET /show" do
-    it "returns http success" do
-      get "/products/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /new" do
-    it "returns http success" do
-      get "/products/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/products/edit"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  pending "right now all the tests are in the controller spec"
 end
