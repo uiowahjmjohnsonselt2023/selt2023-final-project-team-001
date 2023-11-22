901243af6d2f3023661267794ae316b28b64728d
blhohmann <blake-hohmann@uiowa.edu>
2023-11-22 15:01:42 -0600

    fix: update controller to use Current.user

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,19 +1,17 @@
 class StorefrontController < ApplicationController
-  # before_action :authenticate_user! # Assuming you're using Devise for authentication
+  before_action :require_login, except: [:show] # , :index]
 
   def new
-    @user = User.find(session[:user_id])
-    @products = @user.products
-    @storefront = @user.storefront || @user.create_storefront
+    @products = Current.user.products
+    @storefront = Current.user.storefront || Current.user.create_storefront
     @previewed_code = @storefront.custom_code if params[:preview_button]
   end
 
   def create
     # Add logic to handle the 'Preview Custom Code' action
     if params[:storefront] && params[:preview_button]
-      @user = User.find(session[:user_id])
-      @products = @user.products
-      @storefront = @user.storefront || @user.create_storefront
+      @products = Current.user.products
+      @storefront = Current.user.storefront || Current.user.create_storefront
       @previewed_code = params[:storefront][:custom_code]
 
       render turbo_stream: turbo_stream.replace(
@@ -23,17 +21,16 @@ class StorefrontController < ApplicationController
       )
       # render :new
     elsif params[:storefront] && params[:save_button]
-      user = User.find(session[:user_id])
-      storefront = user.storefront || user.create_storefront
+      storefront = Current.user.storefront || Current.user.create_storefront
       storefront.update(custom_code: params[:storefront][:custom_code])
       redirect_to storefront_path(storefront)
     end
   end
 
   def show
-    @user = User.find(session[:user_id])
-    # @products = @user.products
-    @storefront = Storefront.find(params[:id]) # Retrieve the storefront for the current user
+    @storefront = Storefront.find(params[:id])
+    @user = @storefront.user
+    @products = @user.products
     @custom_code = @storefront&.custom_code || "" # Fetch custom code or default to empty string
   end
 end
