a291516e192342dd9a91a47b60deb52a05e6a1e1
powersaudrey25 <powersaudrey25@gmail.com>
2023-11-22 11:58:55 -0600

    feat: update cart and inventory after purchase

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,10 +1,12 @@
 class CheckoutsController < ApplicationController
   before_action :set_current_user, except: [:index]
+  before_action :index, except: [:index]
 
   def index
     cart = Cart.where(user_id: session[:user_id])
 
     @products_in_cart = []
+    @product_ids_and_quantity = []
     @empty = "Your cart is empty!"
 
     if cart.present?
@@ -12,6 +14,7 @@ class CheckoutsController < ApplicationController
         p = Product.find_by(id: product.product_id)
         @current_quantity_of_product = p.quantity
         @products_in_cart.push({name: p.name, price: p.price_cents, quantity: product.quantity, id: product.product_id})
+        @product_ids_and_quantity.push({id: product.product_id, quantity: product.quantity})
       end
     end
   end
@@ -28,5 +31,21 @@ class CheckoutsController < ApplicationController
       flash[:success] = "Item removed from cart."
     end
     redirect_to checkout_path
+  end
+
+  def update_product_inventory
+    if @products_in_cart.empty?
+      flash[:alert] = "You must add products to your cart to purchase them!"
+      render :index
+    else
+      @product_ids_and_quantity.each do |p|
+        # remove from cart and inventory
+        Cart.find_by(user_id: session[:user_id], product_id: p[:id]).destroy
+        product = Product.find_by(id: p[:id])
+        product.update(quantity: (product.quantity - Integer(p[:quantity])))
+      end
+      flash[:notice] = "Order placed successfully!"
+      redirect_to checkout_path
+    end
   end
 end
