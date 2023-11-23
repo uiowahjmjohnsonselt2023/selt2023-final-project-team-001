5892ebed7bf58aedd6d163d98376ab3d39ac17ed
powersaudrey25 <powersaudrey25@gmail.com>
2023-11-23 17:43:44 -0600

    feat: associations for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
+  belongs_to :reviewer, class_name: "User", foreign_key: "reviewer_id"
+  belongs_to :seller, class_name: "User", foreign_key: "seller_id"
   validates :interaction_rating, presence: true, inclusion: {in: 1..5}
 end
