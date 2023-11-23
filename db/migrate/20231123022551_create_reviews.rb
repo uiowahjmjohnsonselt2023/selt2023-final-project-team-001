5e65a5974d053901519afa591458cf7d547d73e8
powersaudrey25 <powersaudrey25@gmail.com>
2023-11-23 15:25:35 -0600

    refactor: make all but description required

--- db/migrate/20231123022551_create_reviews.rb
+++ db/migrate/20231123022551_create_reviews.rb
@@ -1,10 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[7.1]
   def change
     create_table :reviews do |t|
-      t.references :reviewer, index: true, foreign_key: {to_table: :users}
-      t.references :seller, index: true, foreign_key: {to_table: :users}
+      t.references :reviewer, index: true, foreign_key: {to_table: :users}, null: false
+      t.references :seller, index: true, foreign_key: {to_table: :users}, null: false
       t.boolean :has_purchased_from, null: false, default: false
-      t.integer :interaction_rating
+      t.integer :interaction_rating, null: false
       t.text :description
       t.timestamps
     end
