47fe8b1431f344b15d9af2b67fca7e034fbd505c
powersaudrey25 <powersaudrey25@gmail.com>
2023-11-23 20:11:04 -0600

    feat: add description validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :reviewer, class_name: "User", foreign_key: "reviewer_id"
   belongs_to :seller, class_name: "User", foreign_key: "seller_id"
   validates :interaction_rating, presence: true, inclusion: {in: 1..5}
+  validates :description, length: {maximum: 2000, allow_blank: true}
 end
