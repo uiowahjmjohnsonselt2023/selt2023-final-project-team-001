c692cdfe9f14300a2488b47f1b5dc9db3421273d
evfinkn <evan@finken.name>
2023-11-24 19:57:12 -0600

    fix: don't add more than product quantity

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,11 +2,13 @@ class CartsController < ApplicationController
   before_action :require_login
 
   def add_to_cart
+    product = Product.find params[:product_id]
     cart = Cart.find_or_initialize_by(
-      product_id: params[:product_id], user: Current.user
+      product_id: product.id, user: Current.user
     )
     cart.quantity ||= 0
     cart.quantity += params[:quantity].to_i
+    cart.quantity = [cart.quantity, product.quantity].min
     if cart.save
       flash[:notice] = "Item added to cart."
       redirect_to checkout_path
