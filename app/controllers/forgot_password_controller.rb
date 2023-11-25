c14e0bfa0d03bd0e19cfb2c6939efb235e399c5d
powersaudrey25 <powersaudrey25@gmail.com>
2023-11-24 20:31:03 -0600

    fix: an oopsies

--- app/controllers/forgot_password_controller.rb
+++ app/controllers/forgot_password_controller.rb
@@ -1,5 +1,7 @@
 class ForgotPasswordController < ApplicationController
   def new
+    @email = params[:email]
+    @confirm_email = params[:confirm_email]
   end
 
   def send_link
@@ -11,13 +13,13 @@ class ForgotPasswordController < ApplicationController
       render :new
     else
       all_emails = User.pluck(:email)
-      if !all_emails.include?(params[:confirm_email])
+      if all_emails.include?(params[:confirm_email])
         flash[:success] = "Password reset instructions have been sent to your email!"
         redirect_to login_path
       else
         @option_to_signup = true
         flash[:alert] = "There is no account associated with that email."
-        redirect_to forgot_password_path
+        render :new
       end
     end
   end
