4e6a5a376e9b36bcd7c567c6b24f05b83653451d
blhohmann <blake-hohmann@uiowa.edu>
2023-11-28 17:37:39 -0600

    test: change to pending

--- spec/requests/price_alerts_spec.rb
+++ spec/requests/price_alerts_spec.rb
@@ -1,17 +1,5 @@
 require "rails_helper"
 
 RSpec.describe "PriceAlerts", type: :request do
-  describe "GET /send_price_alert" do
-    it "returns http success" do
-      get "/send_price_alert"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /send_test_email" do
-    it "returns http success" do
-      get "/send_test_email"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
