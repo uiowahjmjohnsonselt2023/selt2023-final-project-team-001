4ef58e5b5bcf856f86fd124e12826d47c928d1f4
powersaudrey25 <powersaudrey25@gmail.com>
2023-11-28 21:20:53 -0600

    feat: edit and update view for password reset

--- app/controllers/forgot_password_controller.rb
+++ app/controllers/forgot_password_controller.rb
@@ -12,8 +12,36 @@ class ForgotPasswordController < ApplicationController
       flash[:alert] = "Emails do not match!"
       render :new
     else
+      @user = User.find_by(email: @email)
+      if @user.present?
+        PasswordMailer.with(user: @user).reset.deliver_now
+      end
       flash[:success] = "If an account with that email was found we have sent a link to reset your password!"
       redirect_to login_path
     end
+  end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    flash[:alert] = "Your password reset token has expired, please try again."
+    redirect_to forgot_password_path
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      flash[:success] = "Your password was reset successfully!"
+      redirect_to login_path
+    else
+      flash[:alert] = "Invalid input(s)!"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
