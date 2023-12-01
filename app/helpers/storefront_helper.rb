523eb50e83cdd36c4c15a5152b975a66c041822c
evfinkn <evan@finken.name>
2023-11-30 23:19:08 -0600

    tweak: show search in storefronts index

--- app/helpers/storefront_helper.rb
+++ app/helpers/storefront_helper.rb
@@ -1,2 +1,17 @@
 module StorefrontHelper
+  def storefronts_index_header
+    if params[:search].present?
+      %(Storefronts matching "#{params[:search]}")
+    else
+      "All Storefronts"
+    end
+  end
+
+  def no_products_message
+    if params[:search].present?
+      "No storefronts matched your search."
+    else
+      "There are no storefronts to display."
+    end
+  end
 end
