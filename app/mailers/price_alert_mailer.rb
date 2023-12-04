1add09d19af63d2e8183e33ed19403fb1288c1f4
blhohmann <blake-hohmann@uiowa.edu>
2023-12-04 11:18:55 -0600

    fix: remove comments, and update email contents

--- app/mailers/price_alert_mailer.rb
+++ app/mailers/price_alert_mailer.rb
@@ -1,5 +1,8 @@
 class PriceAlertMailer < ApplicationMailer
-  def send_price_alert(email, product_name, current_price, threshold_price, old_price)
-    mail(to: email, subject: "Price Alert for #{product_name}", body: "Current Price: #{current_price}\nOld Price: #{old_price}\nThreshold Price: #{threshold_price}")
+  def send_price_alert(email, product_id, threshold_price, old_price)
+    @product = Product.find(product_id)
+    product_name = @product.name
+    current_price = @product.price
+    mail(to: email, subject: "Price Alert for #{product_name}", body: "Current Price: #{current_price}\nOld Price: #{old_price}\nThreshold Price: #{threshold_price}\nLink: #{url_for(controller: "products", action: "show", id: product_id, only_path: false)}")
   end
 end
