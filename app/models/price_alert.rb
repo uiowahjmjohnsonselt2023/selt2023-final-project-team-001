3b756c852a9f5bdf537f93cdec31509c5532d5f5
blhohmann <blake-hohmann@uiowa.edu>
2023-12-04 16:06:19 -0600

    feat: only allow one price alert per product for a user

--- app/models/price_alert.rb
+++ app/models/price_alert.rb
@@ -1,4 +1,7 @@
 class PriceAlert < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  validates :product_id, uniqueness: {scope: :user_id, message: "You already have a price alert for this product."}
+  validates :threshold, numericality: {greater_than: 0, message: "Threshold must be greater than 0."}
 end
