a9241e0e8f4622c60fe6ee525e4a4c9779a6b232
blhohmann <blake-hohmann@uiowa.edu>
2023-12-04 01:51:58 -0600

    feat: add old price, threshold price to email

--- app/mailers/price_alert_mailer.rb
+++ app/mailers/price_alert_mailer.rb
@@ -1,9 +1,5 @@
 class PriceAlertMailer < ApplicationMailer
-  def send_price_alert(email, product_name, current_price, threshold_price)
-    @product_name = product_name
-    @current_price = current_price
-    @threshold_price = threshold_price
-
-    mail(to: email, subject: "Price Alert for #{@product_name}", body: "Current Price: #{@current_price}")
+  def send_price_alert(email, product_name, current_price, threshold_price, old_price)
+    mail(to: email, subject: "Price Alert for #{product_name}", body: "Current Price: #{current_price}\nOld Price: #{old_price}\nThreshold Price: #{threshold_price}")
   end
 end
