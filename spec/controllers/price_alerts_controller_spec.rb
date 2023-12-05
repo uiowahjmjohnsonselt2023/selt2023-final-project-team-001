4bd6d2c34c52c26cf5b850c7c010be9d13ead28a
blhohmann <blake-hohmann@uiowa.edu>
2023-12-05 12:37:04 -0600

    test: finish testing for index

--- spec/controllers/price_alerts_controller_spec.rb
+++ spec/controllers/price_alerts_controller_spec.rb
@@ -95,6 +95,13 @@ RSpec.describe PriceAlertsController, type: :controller do
       end
     end
   end
+
+  describe "GET #index" do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
   #
   # describe "GET #show" do
   #
