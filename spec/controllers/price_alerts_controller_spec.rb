67fa0789ddf475496c14d74c42964b5d05283622
blhohmann <blake-hohmann@uiowa.edu>
2023-12-05 08:36:28 -0600

    test: finish testing for new

--- spec/controllers/price_alerts_controller_spec.rb
+++ spec/controllers/price_alerts_controller_spec.rb
@@ -15,6 +15,16 @@ RSpec.describe PriceAlertsController, type: :controller do
         expect(response).to render_template :new
       end
     end
+
+    context "when price alert exists" do
+      it "redirects to the price alert show page" do
+        product = FactoryBot.create(:product)
+        price_alert = FactoryBot.create(:price_alert, product: product, user: user)
+        get :new, params: {product_id: product.id}
+        expect(response).to redirect_to price_alert_path(price_alert.id)
+        expect(flash[:alert]).to eq I18n.t("price_alerts.new.already_exists")
+      end
+    end
   end
 
   # describe "POST #create" do
