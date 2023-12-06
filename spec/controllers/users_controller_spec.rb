a45b653d100fb74c80ff0bf6aa2a220866949ff9
evfinkn <evan@finken.name>
2023-12-06 10:04:04 -0600

    refactor: use user factories

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,31 +1,11 @@
 require "rails_helper"
 
 RSpec.describe UsersController, type: :controller do
-  let(:valid_user) do
-    {
-      first_name: "John",
-      last_name: "Doe",
-      email: "john.doe@example.com",
-      password: "password123!P",
-      password_confirmation: "password123!P"
-    }
-  end
-
-  let(:invalid_user) do
-    {
-      first_name: "John",
-      last_name: "Doe",
-      email: "john.doeexample.com",
-      password: "pass",
-      password_confirmation: "password123"
-    }
-  end
-
   describe "POST #create" do
     context "with valid attributes" do
       it "creates a new user and redirects" do
         expect {
-          post :create, params: {user: valid_user}
+          post :create, params: {user: attributes_for(:user)}
         }.to change(User, :count).by(1)
         expect(response).to redirect_to(login_path)
         expect(flash[:notice]).to eq("Sign up successful!")
@@ -35,7 +15,7 @@ RSpec.describe UsersController, type: :controller do
     context "with invalid attributes" do
       it "does not creates a new user" do
         expect {
-          post :create, params: {user: invalid_user}
+          post :create, params: {user: attributes_for(:user, password: "pass")}
         }.to_not change(User, :count)
         expect(response).to render_template(:new)
         expect(flash[:alert]).to eq("Invalid input(s)!")
