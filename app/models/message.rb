e5acc218549c5f7f1048557024b3932a410bac97
powersaudrey25 <powersaudrey25@gmail.com>
2023-12-07 14:45:07 -0600

    fix: typo

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
-  belongs_to :sender, class_name: "Message", foreign_key: "sender_id"
-  belongs_to :receiver, class_name: "Message", foreign_key: "receiver_id"
+  belongs_to :sender, class_name: "User", foreign_key: "sender_id"
+  belongs_to :receiver, class_name: "User", foreign_key: "receiver_id"
 
   validates :sender_id, presence: true
   validates :receiver_id, presence: true
