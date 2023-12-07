bd188abaaecd628181069937f3b8bfcf36e8113f
powersaudrey25 <powersaudrey25@gmail.com>
2023-12-07 14:04:32 -0600

    feat: messages model

---
+++ app/models/message.rb
@@ -0,0 +1,9 @@
+class Message < ApplicationRecord
+  belongs_to :sender, class_name: "Message", foreign_key: "sender_id"
+  belongs_to :receiver, class_name: "Message", foreign_key: "receiver_id"
+
+  validates :sender_id, presence: true
+  validates :receiver_id, presence: true
+  validates :message, presence: true, length: {maximum: 500}
+  validates :subject, presence: true, length: {maximum: 100}
+end
