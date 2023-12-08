8c4abc92d7faf97e339ab6dde9a6375648138580
evfinkn <evan@finken.name>
2023-12-08 10:33:08 -0600

    feat: add seller and promotions associations to carts

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,8 @@
 class Cart < ApplicationRecord
   belongs_to :user
   belongs_to :product
+  has_one :seller, through: :product
+  has_many :promotions, through: :product
 
   validates :quantity, numericality: {greater_than: 0, only_integer: true}
 end
