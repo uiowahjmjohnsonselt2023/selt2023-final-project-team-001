3fe5954feb785db5021c784e6035a1bf12f10456
evfinkn <evan@finken.name>
2023-12-08 10:42:36 -0600

    feat: add methods for testing if promotion is eligible

--- app/models/promotions/promotionable.rb
+++ app/models/promotions/promotionable.rb
@@ -5,4 +5,16 @@ module Promotionable
     has_one :promotion, as: :promotionable, touch: true
     delegate_missing_to :promotion
   end
+
+  def product_eligible?(cart)
+    if products.empty?
+      cart.product.seller == seller
+    else
+      products.include?(cart.product)
+    end
+  end
+
+  def eligible?(cart)
+    active? && product_eligible?(cart)
+  end
 end
