bcb14b768008da0d83af7a1b2d945fd27e5eba2b
powersaudrey25 <powersaudrey25@gmail.com>
2023-12-09 13:19:01 -0600

    test: rspecs and cleanup

---
+++ spec/controllers/messages_controller_spec.rb
@@ -0,0 +1,76 @@
+require "rails_helper"
+
+describe MessagesController, type: :controller do
+  let(:user) { create(:user) }
+  let(:profile) { create(:profile, user: user) }
+  let(:another_user) { create(:user) }
+
+  before do
+    allow(controller).to receive(:require_login)
+    allow(Current).to receive(:user).and_return(user)
+  end
+
+  describe "new" do
+    it "redirects to root path if user tries to send a message to themselves and have profile" do
+      get :new, params: {profile_id: profile.id, receiver_id: user.id}
+
+      expect(response).to redirect_to(profile_path(profile))
+      expect(flash[:alert]).to eq("You cannot send a message to yourself.")
+    end
+  end
+
+  describe "create" do
+    context "with valid parameters" do
+      it "creates a new message" do
+        expect do
+          post :create, params: {profile_id: profile.id, receiver_id: another_user.id, message: {subject: "test", message: "test"}}
+        end.to change(Message, :count).by(1)
+      end
+
+      it "redirects to the profile path" do
+        post :create, params: {profile_id: profile.id, receiver_id: another_user.id, message: {subject: "test", message: "test"}}
+
+        expect(response).to redirect_to(profile_path(profile))
+        expect(flash[:success]).to eq("Message successfully sent!")
+      end
+    end
+
+    context "with invalid parameters" do
+      it 'renders "new" template with unprocessable entity status' do
+        post :create, params: {profile_id: profile.id, receiver_id: another_user.id, message: {subject: "Invalid Message"}}
+
+        expect(response).to render_template("new")
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe "show" do
+    let(:message) { create(:message, sender_id: another_user.id, receiver_id: user.id, sender_name: another_user.full_name, receiver_name: user.full_name, subject: "test", message: "test") }
+
+    it "updates the message status based on the user role" do
+      get :show, params: {message_id: message.id}
+
+      expect(assigns(:message).hasRead).to be_truthy
+    end
+  end
+
+  describe "delete" do
+    let(:message) { create(:message, sender_id: another_user.id, receiver_id: user.id, sender_name: another_user.full_name, receiver_name: user.full_name, subject: "test", message: "test") }
+
+    context 'with confirmation "yes"' do
+      it "soft-deletes the message" do
+        post :delete, params: {message_id: message.id, confirmation: "yes"}
+
+        expect(message.reload.hasReceiverDeleted).to be_truthy
+        expect(flash[:success]).to eq("Message deleted.")
+      end
+
+      it "redirects to view_messages_path" do
+        post :delete, params: {message_id: message.id, confirmation: "yes"}
+
+        expect(response).to redirect_to(view_messages_path)
+      end
+    end
+  end
+end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,39 @@
 require "rails_helper"
 
-RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Message, type: :model do
+  let(:user) { create(:user) }
+
+  context "with valid attributes" do
+    it "is valid" do
+      message = build(:message, sender: user, receiver: user, sender_name: "joe", receiver_name: "jane", subject: "test", message: "test")
+      expect(message).to be_valid
+    end
+  end
+
+  context "with missing sender information" do
+    it "is invalid" do
+      message = build(:message, sender_id: nil, sender_name: nil)
+      expect(message).to be_invalid
+      expect(message.errors[:sender_id]).to include("can't be blank")
+      expect(message.errors[:sender_name]).to include("can't be blank")
+    end
+  end
+
+  context "with missing receiver information" do
+    it "is invalid" do
+      message = build(:message, receiver_id: nil, receiver_name: nil)
+      expect(message).to be_invalid
+      expect(message.errors[:receiver_id]).to include("can't be blank")
+      expect(message.errors[:receiver_name]).to include("can't be blank")
+    end
+  end
+
+  context "with missing message information" do
+    it "is invalid" do
+      message = build(:message, subject: nil, message: nil)
+      expect(message).to be_invalid
+      expect(message.errors[:subject]).to include("can't be blank")
+      expect(message.errors[:message]).to include("can't be blank")
+    end
+  end
 end
