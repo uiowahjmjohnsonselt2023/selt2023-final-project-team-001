cf7d0cf224fe5acc4933a619254f3d4a128bfd93
evfinkn <evan@finken.name>
2023-12-09 11:21:21 -0600

    feat: add default scope to CartItem

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -5,5 +5,9 @@ class CartItem < ApplicationRecord
   has_one :seller, through: :product
   has_many :promotions, through: :product
 
+  # Always eager load product since we almost always need it.
+  # This way, we avoid n+1 queries.
+  default_scope { includes(:product) }
+
   validates :quantity, numericality: {greater_than: 0, only_integer: true}
 end
