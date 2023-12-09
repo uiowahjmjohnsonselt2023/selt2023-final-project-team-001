8e1f783f821bdbe4573b3b912a1830e685fba995
evfinkn <evan@finken.name>
2023-12-09 11:33:09 -0600

    feat: more helpers for Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,6 +2,9 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
+  has_many :promotions, through: :products
+
+  delegate :empty?, :size, :length, to: :cart_items
 
   def subtotal
     cart_items.sum(&:subtotal)
