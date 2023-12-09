c5e3b96ba82074391c9ef54991b106999fe6f4ba
evfinkn <evan@finken.name>
2023-12-08 22:12:55 -0600

    refactor: change percentage to decimal

--- app/models/promotions/percent_off.rb
+++ app/models/promotions/percent_off.rb
@@ -1,5 +1,5 @@
 class Promotions::PercentOff < ApplicationRecord
   include Promotionable
 
-  validates :percentage, numericality: {greater_than: 0, less_than_or_equal_to: 100}
+  validates :percentage, numericality: {greater_than: 0, less_than_or_equal_to: 1}
 end
