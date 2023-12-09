d63b9e33cfe158f44b9d91a13d6706f2e9b0eace
evfinkn <evan@finken.name>
2023-12-09 11:23:44 -0600

    feat: add subtotal helpers to CartItem and Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,8 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
+
+  def subtotal
+    cart_items.sum(&:subtotal)
+  end
 end
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -5,9 +5,15 @@ class CartItem < ApplicationRecord
   has_one :seller, through: :product
   has_many :promotions, through: :product
 
+  delegate :price, to: :product # for convenience
+
   # Always eager load product since we almost always need it.
   # This way, we avoid n+1 queries.
   default_scope { includes(:product) }
 
   validates :quantity, numericality: {greater_than: 0, only_integer: true}
+
+  def subtotal
+    quantity * price
+  end
 end
