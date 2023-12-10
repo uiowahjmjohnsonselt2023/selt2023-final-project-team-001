d998897aec4101e4eacd3046ee6098928e33cc81
evfinkn <evan@finken.name>
2023-12-09 21:39:34 -0600

    fix: don't submit attributes for other promo types

--- app/javascript/controllers/promotion_form_controller.js
+++ app/javascript/controllers/promotion_form_controller.js
@@ -24,7 +24,12 @@ export default class extends Controller {
             this.typeFormTargets.map((el) => el.dataset.type)
         );
         this.typeFormTargets.forEach((el) => {
-            el.classList.toggle("d-none", el.dataset.type !== type)
+            el.classList.toggle("d-none", el.dataset.type !== type);
+            Array.from(el.getElementsByTagName("input")).forEach((input) => {
+                // Disable all inputs that are not in the current type form
+                // so that they are not submitted with the form.
+                input.disabled = el.dataset.type !== type;
+            });
         });
     }
 }
