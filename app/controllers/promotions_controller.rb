925099d05d49836722da91066853fc6a03c26d3f
evfinkn <evan@finken.name>
2023-12-09 19:58:19 -0600

    feat: add default datetimes for promotions form

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -4,13 +4,23 @@ class PromotionsController < ApplicationController
 
   def new
     @promotion = Promotion.new
+    set_times
   end
 
   def edit
     @promotion = Promotion.find params[:id]
+    set_times
     if Current.user != @promotion.seller && !Current.user.is_admin
       flash[:alert] = "You don't have permission to edit that promotion."
       redirect_to root_path
     end
+  end
+
+  private
+
+  def set_times
+    @now = Time.now
+    @midnight = @now.next_day.midnight
+    @week_from_midnight = @midnight.advance weeks: 1
   end
 end
