e49ff26d5bfe961fc73b9f1570407e1c3111c482
evfinkn <evan@finken.name>
2023-12-09 20:10:19 -0600

    feat: add promotions create route

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -7,6 +7,16 @@ class PromotionsController < ApplicationController
     set_times
   end
 
+  def create
+    @promotion = Current.user.promotions.build(promotion_params)
+    if @promotion.save
+      redirect_to @promotion
+    else
+      flash[:alert] = "Please fix the errors below."
+      render "new", status: :unprocessable_entity
+    end
+  end
+
   def edit
     @promotion = Promotion.find params[:id]
     set_times
@@ -22,5 +32,19 @@ class PromotionsController < ApplicationController
     @now = Time.now
     @midnight = @now.next_day.midnight
     @week_from_midnight = @midnight.advance weeks: 1
+  end
+
+  def promotion_params
+    params.require(:promotion).permit(
+      :name,
+      :description,
+      :starts_at,
+      :ends_at,
+      :min_quantity,
+      :max_quantity,
+      :promotionable_type,
+      promotionable_attributes: {},
+      product_ids: []
+    )
   end
 end
