11d4b5a00fddf0c4bb8fa1b35eb7a23dcf74b20b
evfinkn <evan@finken.name>
2023-12-10 01:04:38 -0600

    fix: remove_from_cart and update_product_inventory

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -3,8 +3,8 @@ class CheckoutsController < ApplicationController
   before_action :index, except: [:index]
 
   def index
-    cart = Cart.find_or_create_by(user: Current.user)
-    cart_items = cart.apply_promotions
+    @cart = Cart.find_or_create_by(user: Current.user)
+    cart_items = @cart.apply_promotions
 
     @applied_promotions = cart_items.lazy
       .reject { |ci| ci.applied_promotion.nil? }
@@ -28,8 +28,8 @@ class CheckoutsController < ApplicationController
       {id: cart_item.product_id, quantity: cart_item.product.quantity}
     end
 
-    @cart_original_price = cart.subtotal
-    @cart_price = cart.discounted_subtotal
+    @cart_original_price = @cart.subtotal
+    @cart_price = @cart.discounted_subtotal
     @empty = "Your cart is empty!"
   end
 
@@ -44,21 +44,21 @@ class CheckoutsController < ApplicationController
 
   def remove_from_cart
     if params[:confirmation] == "yes"
-      CartItem.find_by(user_id: session[:user_id], product_id: params[:product_id]).destroy
+      CartItem.find(params[:cart_item_id]).destroy
       flash[:success] = "Item removed from cart."
     end
     redirect_to checkout_path
   end
 
   def update_product_inventory
-    if @products_in_cart.empty?
+    if @cart.empty?
       flash[:alert] = "You must add products to your cart to purchase them!"
       render :index
     else
-      @product_ids_and_quantity.each do |p|
-        CartItem.find_by(user_id: session[:user_id], product_id: p[:id]).destroy
-        product = Product.find_by(id: p[:id])
-        product.update!(quantity: (product.quantity - Integer(p[:quantity])))
+      @cart.cart_items.each do |cart_item|
+        product = cart_item.product
+        product.update!(quantity: (product.quantity - cart_item.quantity))
+        cart_item.destroy
       end
       flash[:notice] = "Order placed successfully!"
       redirect_to checkout_path
