e3f80a00b1cb50b041eb64972254633afe778659
kesslerjohn <john-kessler@uiowa.edu>
2023-12-09 22:27:25 -0600

    tweak: redirect to home page on failed request

--- app/controllers/storefronts_controller.rb
+++ app/controllers/storefronts_controller.rb
@@ -65,13 +65,15 @@ class StorefrontsController < ApplicationController
     @user = Current.user
     # threshold 3 stars
     # threshold 5 reviews
-    # @user.profile.update(seller_rating: 2)
+    @user.profile.update(seller_rating: 2)
     puts @user.profile.seller_rating
     unless @user.profile.seller_rating.nil?
       if @user.profile.seller_rating < 3
         flash[:warning] = "Your seller rating is too low to set up a storefront at this time. Sellers must have a rating of at least 3 stars to set up a store front."
+        redirect_to root_path and return
       elsif Review.where(seller_id: @user.id).count < 5
         flash[:warning] = "You do not have enough reviews to set up a storefront. You must have at least 5 reviews"
+        redirect_to root_path and return
       else
         @admin = User.create(first_name: "test", last_name: "admin", email: "jkkessler95@gmail.com", is_admin: true)
         StorefrontRequestMailer.with(user: @admin).request_approval.deliver_now
